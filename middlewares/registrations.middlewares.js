fab0dcf84578e4dccb9be5f650878ff189c6d92e
jdgc14 <joseojedapro@gmail.com>
2022-09-02 15:52:36 -0500

    fixing middleware registrationIsActive

--- middlewares/registrations.middlewares.js
+++ middlewares/registrations.middlewares.js
@@ -36,6 +36,7 @@ const registrarionIsActive = async (req, res, next) => {
     } catch (err) {
         console.log(err)
     }
+    next()
 }
 
 module.exports = { findRegistration, registrarionIsActive }
