a469a2716823587eaed337b582d735da51143b31
jdgc14 <joseojedapro@gmail.com>
2022-09-03 17:07:41 -0500

    adding database cloud

--- utils/database.util.js
+++ utils/database.util.js
@@ -2,13 +2,16 @@ const { Sequelize, DataTypes } = require('sequelize')
 
 // Create a database instance
 const db = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '1234abcd',
-    port: 5432,
-    database: 'attendances',
-    logging: false, // false to disable logging clear console
+    dialect: 'mysql',
+    host: 'us-east.connect.psdb.cloud',
+    username: 'wpwlquhwtvzi8st7abtf',
+    password: 'pscale_pw_fKUgugnU3IgbPAL4gTgp9yho0RQblU9h7Wz6xVGP75k',
+    // port: 5432,
+    database: 'evolutec',
+    logging: false, // false to clear console
+    dialectOptions: {
+        ssl: { rejectUnauthorized: true },
+    },
 })
 
 module.exports = { db, DataTypes }
